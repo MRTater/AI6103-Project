5ef68d4b4e2fee00ff8975e76a2c36ae7896f5c0
akazfu <akazfu@outlook.com>
2023-04-18 09:58:13 +0800

    ..

--- run.sh
+++ run.sh
@@ -17,10 +17,9 @@ python train.py \
 --batch_size 16 \
 --T 1000 \
 --dataset "/home/msai/zfu009/dataset/pokemon_jpg/pokemon_jpg" \
---epochs 2000 \
+--epochs 1000 \
 --beta_schedule cosine \
 --activation silu \
-<<<<<<< HEAD
 --use_self_attention \
 --lr_scheduler cosine \
 # --resume_from "/home/msai/zfu009/workspace/AI6103-Project/models/1.pth"
